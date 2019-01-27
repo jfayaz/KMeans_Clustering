7220fb86aac7e553db48b43b33bd82c5ff7f0256
jfayaz <jfayaz@uci.edu>
2019-01-26 21:31:40 -0800

    added website

--- KMeans___code_by_Jawad_Fayaz.m
+++ KMeans___code_by_Jawad_Fayaz.m
@@ -1,6 +1,7 @@
 clear ; close all; clc; addpath('functions')
 %% ====================== KMEANS CLUSTERING ============================ %%
 %  written by : JAWAD FAYAZ (email: jfayaz@uci.edu)
+%  visit: (https://jfayaz.github.io)
 
 %  ------------- Instructions -------------- %
 %  INPUT:
